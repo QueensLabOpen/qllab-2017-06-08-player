3d4297262f7d21b0e36b0c7fe7e22a25426394e1
Felix Svenssin <klowdo.fs@gmail.com>
2017-06-08 22:19:55 +0200

    Added css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-  entry: "./index.js",
-  output: {
-    filename: "bundle.js",
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "babel-loader"
-    }]
-  }
+    entry: "./index.js",
+    output: {
+        filename: "bundle.js",
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
+    }
 }
